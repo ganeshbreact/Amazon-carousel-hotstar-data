01da082b2fe4183b61600384dad1f56e889e532b
ganeshbreact <ganesh.b.react@gmail.com>
2024-07-03 00:05:48 +0530

    disney carousel with counters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Data, { imgLinks } from "./components/Data";
+import { useState } from 'react';
 
 function App() {
+  const[index,setIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+<div className="App" style={{display:"flex",flexDirection:"row"}}>
+
+<div style={{display:"flex",alignItems:"center"}}>
+<button style={{height:"50px", backgroundColor:"darkGrey"}}
+onClick={() =>{
+  if(index > 0 )
+  setIndex(index-1);
+else
+setIndex(imgLinks.length-1);
+
+
+}}
+>&lt;</button>
+
+</div>
+
+
+<img width="96%" height="500px" src={imgLinks[index].url} alt="" />
+
+<div style={{display:"flex",alignItems:"center",}}>
+  <button style={{height:"50px",backgroundColor:"darkGrey"}}
+   onClick={() =>{
+
+    if( index < imgLinks.length-1)
+      setIndex(index+1);
+   else 
+   setIndex(0);
+    
+  }}
+  >&gt;</button>
+
+</div>
+
+
+
+</div>
+<div style={{display:"flex",justifyContent:"center"}}>
+<h1>{imgLinks[index].imgNo}</h1>
+
+</div>
     </div>
   );
 }
---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export  const imgLinks = [
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/8862/1719747698862-i",imgNo :"Image-1"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/959/1719480660959-i",imgNo :"Image-2"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/679/1719501060679-i", imgNo :"Image-3"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/2455/1717523662455-i", imgNo :"Image-4"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/5727/1717073895727-i", imgNo :"Image-5"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/6725/1719628836725-i", imgNo :"Image-6"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/9312/1719747899312-i", imgNo :"Image-7"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/8196/1718883338196-i", imgNo :"Image-8"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/6312/1714673666312-i", imgNo :"Image-9"},
+    {url:"https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/8776/1711007678776-i", imgNo :"Image-10"},
+    
+];
+
